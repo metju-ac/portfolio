e31bbc5e8a5a3b6912eae9ca7a1365338d7dd1f6
Matej Klima <matej.klima5@gmail.com>
2026-02-27 23:21:18 +0100

    update About page with Matěj's bio and attribution to original project

--- src/components/Windows/Documents/About.vue
+++ src/components/Windows/Documents/About.vue
@@ -10,9 +10,6 @@
       <p>{{ $t('windows.documents.about.contextSecondParagraph') }}</p>
       <p>{{ $t('windows.documents.about.contextThirdParagraph') }}</p>
       <p>{{ $t('windows.documents.about.contextFourthParagraph') }}</p>
-      <p>{{ $t('windows.documents.about.contextFifthParagraph') }}</p>
-      <p>{{ $t('windows.documents.about.contextSixthParagraph') }}</p>
-      <p>{{ $t('windows.documents.about.contextSeventhParagraph') }}</p>
     </div>
     <h1 class="font-semibold">{{ $t('windows.documents.about.secondTitle') }}</h1>
     <hr class="mt-2 mb-2 border-t-1 border-black" />
@@ -21,7 +18,6 @@
       <li>{{ $t('windows.documents.about.helpSecond') }}</li>
       <li>{{ $t('windows.documents.about.helpThird') }}</li>
       <li>{{ $t('windows.documents.about.helpFourth') }}</li>
-      <li>{{ $t('windows.documents.about.helpFifth') }}</li>
     </ul>
   </section>
 </template>
